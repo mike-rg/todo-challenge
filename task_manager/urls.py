d0a19bc954bd9f075e6574d9657cd91a79400727
Migue Maldonado <miguel.alberto.maldonado@gmail.com>
2023-06-13 14:23:54 -0300

    build(update-project): configure jwt token

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
@@ -9,4 +10,6 @@ urlpatterns = [
     path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('docs/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('redocs/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/v1/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]

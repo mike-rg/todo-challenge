2edb954ca5685703e812f4d5994918bd8928e44e
Migue Maldonado <miguel.alberto.maldonado@gmail.com>
2023-06-15 01:30:27 -0300

    fix(update-accounts-app): fix set user email in manager. set is_active in true by default and add migrations

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
+from apps.accounts.helpers import send_email_verification
+
 from .models import User
 
 
+def resend_email_verification(modeladmin, request, queryset):
+    for user in queryset.filter(email_verified=False, is_active=False):
+        send_email_verification(user)
+
+
+resend_email_verification.short_description = "Resend confirmation email"
+
+
 class UserAdmin(BaseUserAdmin):
-    pass
+    list_display = ['username', 'email', 'is_staff', 'is_active', 'email_verified']
+    actions = [resend_email_verification]
 
 
 admin.site.register(User, UserAdmin)

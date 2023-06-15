6bba9dc24267d73258fb36b5ac971f830036236c
Migue Maldonado <miguel.alberto.maldonado@gmail.com>
2023-06-15 10:54:30 -0300

    feat(update-assignments-app): create assignments models and add migrations

--- apps/assignments/models.py
+++ apps/assignments/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from apps.base.models import BaseModel
+from .constants import AssignmentStatus
+
+
+class Assignment(BaseModel):
+    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+    status = models.CharField(
+        max_length=30, choices=AssignmentStatus.STATUS_CHOICES, default=AssignmentStatus.PENDING)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    due_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.user} - {self.title}"

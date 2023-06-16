824c7995eabceeabfc84582c5a105914ee9ef6b5
Migue Maldonado <miguel.alberto.maldonado@gmail.com>
2023-06-16 04:17:25 -0300

    build(update-project): add permission classes, change .env_example file and rename github actions files

--- task_manager/settings/base.py
+++ task_manager/settings/base.py
@@ -150,7 +150,10 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 
 
@@ -167,7 +170,7 @@ SPECTACULAR_SETTINGS = {
 JWT_SIGNING_KEY = os.getenv('JWT_SIGNING_KEY')
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,

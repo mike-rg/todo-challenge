01ea215022882cac5fd2468dc8bfba111f76ddbb
Migue Maldonado <miguel.alberto.maldonado@gmail.com>
2023-06-16 05:36:35 -0300

    feat(update-project): add postgresql service, celery worker and redis

--- task_manager/settings/base.py
+++ task_manager/settings/base.py
@@ -93,11 +93,14 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task_manager_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'task-manager-db',
+        'PORT': 5432,
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -208,3 +211,10 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
+
+# Celery
+CELERY_QUEUE_NAME_PREFIX = None
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
